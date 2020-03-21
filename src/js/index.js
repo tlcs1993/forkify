dc420e587e6e0d732ecdde013f98a8b696bb71a6
Thiago Cruz <tlcs1993@gmail.com>
2020-03-21 16:01:04 -0300

    After class 142 "Building the Search Controller"

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,40 @@
-import axios from 'axios';
+import Search from './../models/Search';
 
-async function getResults(query) {
-    try {
-        const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${query}`);
-        const recipes = res.data.recipes;
-        console.log(recipes);
-    } catch (err) {
-        alert(err);
+/* Estado global da aplicação.
+
+    * - Objeto de busca 
+    * - Objeto da receita atual
+    * - Objeto da lista de compras
+    * - Receitas curtidas
+
+*/
+
+const state = {};
+
+const controlSearch = async () => {
+    // FIXME: 1. Busca a 'query' a partir da 'view'.
+    const query = 'pizza';
+
+    // Caso a 'query' exista...
+    if (query) {
+        // 2. Cria um novo objeto de busca e o adiciona ao estado.
+        state.search = new Search(query);
+
+        // TODO: 3. Prepara a interface de usuário (UI) para os resultados.
+
+        // 4. Procura por receitas.
+        await state.search.getResults();
+
+        // FIXME: 5. Renderiza os resultados na iterface de usuário (UI).
+        console.log(state.search.result);
     }
 }
 
-getResults('pizza');
+document.querySelector('.search').addEventListener('submit', (e) => {
+    e.preventDefault();
+
+});
+
+const search = new Search('pizza');
+console.log(search);
+search.getResults();
